a0fae4aee52289f8d539eedcea3647d7f3e6ad06
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-08 05:35:08 +0530

    Master branch code commit

---
+++ src/orders/LoginClass_OrderApp.java
@@ -0,0 +1,12 @@
+package orders;
+
+public class LoginClass_OrderApp 
+{
+	public void orderAppLoginMethod()
+	{
+		System.out.println("This is orderApplogin Method logic");
+		System.out.println("Enter the username");
+		System.out.println("Enter the Password");
+		System.out.println("User Successfully logged in");
+	}
+}
---
+++ src/orders/OrdersDetails.java
@@ -0,0 +1,10 @@
+package orders;
+
+public class OrdersDetails 
+{
+	public void getOrderDetails()
+	{
+		System.out.println("Order details will show current, past and future info");
+	}
+
+}
